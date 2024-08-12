9680a589ebb9b9f6c2206e2bd69f5560bca77244
Luke <l.ukefarrellyspain@.com>
2024-08-12 15:12:54 +0200

    Refinements

--- components/widgets.js
+++ components/widgets.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const AppTray = () => {
   const [widgets, setWidgets] = useState([]);
-  const intervalRef = useRef(null);
 
   const createWidget = (content, id) => {
     const newWidget = {
       id: id || Date.now(), // Use provided ID or generate a new one
       content: content,
     };
-
-    setWidgets(prevWidgets => {
-      if (prevWidgets.length < 4) {
-        return [...prevWidgets, newWidget];
-      } else {
-        // Remove the oldest widget and add the new one
-        return [...prevWidgets.slice(1), newWidget];
-      }
-    });
+    if (widgets.length < 4) {
+      setWidgets(prevWidgets => [...prevWidgets, newWidget]);
+    } else {
+      // Remove the oldest widget after a short delay
+      setTimeout(() => {
+        setWidgets(prevWidgets => prevWidgets.slice(1)); // Remove the oldest widget
+        // Add the new widget after the delay
+        setTimeout(() => {
+          setWidgets(prevWidgets => [...prevWidgets, newWidget]);
+        }, 230); // Adjust the delay as needed for smooth animation
+      }, 10); // Adjust the delay as needed for smooth animation
+    }
   };
+  
 
+useEffect(() => {
   const fetchWeatherData = async () => {
     try {
       const cacheKey = 'weatherData';
@@ -30,6 +34,7 @@ const AppTray = () => {
       const cachedData = JSON.parse(localStorage.getItem(cacheKey));
       if (cachedData && currentTime - cachedData.timestamp < cacheExpiry) {
         console.log("Using cached weather data");
+
         createWeatherWidget(cachedData.data);
         return;
       }
@@ -43,57 +48,62 @@ const AppTray = () => {
       };
       localStorage.setItem(cacheKey, JSON.stringify(newData));
 
-      createWeatherWidget(data);
+     createWeatherWidget(data);
     } catch (error) {
       console.error('Error fetching or caching weather data:', error);
     }
   };
 
-  const createWeatherWidget = (weatherData) => {
-    try {
-      const icon = weatherData.weather[0].icon;
-      const temp = Math.trunc(weatherData.main.temp);
-      const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  fetchWeatherData();
 
-      const existingWeatherWidget = widgets.find(widget => widget.id === 'weather');
+  // Set interval to fetch new weather data every 15 minutes
+  const intervalId = setInterval(fetchWeatherData, 15 * 60 * 1000);
 
-      if (existingWeatherWidget) {
-        const existingIcon = existingWeatherWidget.content.props.children[0].props.src;
-        const existingTemp = existingWeatherWidget.content.props.children[1].props.children[0];
+  // Clean up interval on component unmount
+  return () => clearInterval(intervalId);
+}, [widgets]);
 
-        if (iconUrl !== existingIcon || temp !== parseInt(existingTemp)) {
-          removeWidget("weather");
-          createWidgetWeatherContent(iconUrl, temp);
-        }
-      } else {
-        createWidgetWeatherContent(iconUrl, temp);
-      }
-    } catch (error) {
-      console.error('Error creating weather widget:', error);
+
+
+const createWeatherWidget = (weatherData) => {
+  try {
+    const icon = weatherData.weather[0].icon;
+    const temp = Math.trunc(weatherData.main.temp);
+	
+	const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+
+    const existingWeatherWidget = widgets.find(widget => widget.id === 'weather');
+
+    if (existingWeatherWidget) {
+      const existingIcon = existingWeatherWidget.content.props.children[0].props.src;
+      const existingTemp = existingWeatherWidget.content.props.children[1].props.children[0];
+	 
+	if (iconUrl !== existingIcon || temp !== parseInt(existingTemp)) {
+        removeWidget("weather");
+      } 
+    } else {
+
+      const weatherContent = (
+        <div className="flex justify-center items-center max-h-full w-full" id="weather">
+          <img className="h-16" src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="Weather Icon" />
+          <div className="text-xl font-semibold text-center text-zinc-800">{temp}°C</div>
+        </div>
+      );
+
+      createWidget(weatherContent, 'weather');
     }
-  };
+  } catch (error) {
+    console.error('Error creating weather widget:', error);
+  }
+};
 
-  const createWidgetWeatherContent = (iconUrl, temp) => {
-    const weatherContent = (
-      <div className="flex px-2 justify-center items-center max-h-full w-full" id="weather">
-        <img className="h-16" src={iconUrl} alt="Weather Icon" />
-        <div className="text-xl font-semibold text-center text-zinc-800">{temp}°C</div>
-      </div>
-    );
-    createWidget(weatherContent, 'weather');
-  };
+
 
   const removeWidget = (id) => {
     setWidgets(prevWidgets => prevWidgets.filter(widget => widget.id !== id));
   };
 
-  useEffect(() => {
-    fetchWeatherData();
-
-    intervalRef.current = setInterval(fetchWeatherData, 15 * 60 * 1000);
-
-    return () => clearInterval(intervalRef.current);
-  }, []);
 
   return (
       <div className="z-50 w-full h-16 flex justify-evenly" id="appTray">
